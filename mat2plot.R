2d0eec12d1ef80d111e2a5e2f93c5fcaa392d86e
Qin Lin <47169253+Ci-TJ@users.noreply.github.com>
2025-03-28 17:30:12 +0800

    Update mat2plot.R

--- mat2plot.R
+++ mat2plot.R
@@ -125,6 +125,8 @@ mat2plot <- function(project=c("TCGA-LUSC"), data_dir="./GDCdata", num_tp=100, n
   }
 #test
 #linshi <- mat2plot(data_dir = "../GDCdata/", is_short = TRUE)
+#project=c("TCGA-LUSC"); data_dir="../GDCdata"; num_tp=100; num_nt=100;tp_t="TP"; tp_n="NT"; 
+#is_short=TRUE; save=TRUE; target=c("FAM135B"); candidate="FAM135B",p=project[1]
     
 
                             
