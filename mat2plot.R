859ea44145eb067a0f025a614fdb7c1f740dadbe
Qin Lin <47169253+Ci-TJ@users.noreply.github.com>
2025-03-29 14:18:16 +0800

    Update mat2plot.R

--- mat2plot.R
+++ mat2plot.R
@@ -20,170 +20,178 @@ mat2plot <- function(project=c("TCGA-LUSC"), data_dir="./GDCdata", num_tp=100, n
     if (file.exists(file.path("tmp", p)) == FALSE){
       dir.create(file.path("tmp", p), recursive = TRUE)
       }
-        
-    query <- GDCquery(project = p,
+    #######
+    if (file.exists(file.path("tmp", p, paste0(p,"_exp.csv")) == FALSE)){
+      query <- GDCquery(project = p,
                       data.category = "Transcriptome Profiling",
                       data.type = "Gene Expression Quantification", 
                       workflow.type = "STAR - Counts")
-    samplesDown <- getResults(query,cols=c("cases"))
-    #tumor samples
-    dataSmTP <- TCGAquery_SampleTypes(barcode = samplesDown,
+      samplesDown <- getResults(query,cols=c("cases"))
+      #tumor samples
+      dataSmTP <- TCGAquery_SampleTypes(barcode = samplesDown,
                                   typesample = tp_t)
-    #normal samples
-    dataSmNT <- TCGAquery_SampleTypes(barcode = samplesDown,
+      #normal samples
+      dataSmNT <- TCGAquery_SampleTypes(barcode = samplesDown,
                                   typesample = tp_n)
-    if (is_short == TRUE){
-      if (all(grepl("TCGA",dataSmNT)) & all(grepl("TCGA",dataSmNT))){
-        dataSmTP_short <- dataSmTP[1:ifelse(num_tp <= length(dataSmTP), num_tp, length(dataSmTP))]
-        dataSmNT_short <- dataSmNT[1:ifelse(num_nt <= length(dataSmNT), num_nt, length(dataSmNT))]
-        queryDown <- GDCquery(project = p, 
+      if (is_short == TRUE){
+        if (all(grepl("TCGA",dataSmNT)) & all(grepl("TCGA",dataSmNT))){
+          dataSmTP_short <- dataSmTP[1:ifelse(num_tp <= length(dataSmTP), num_tp, length(dataSmTP))]
+          dataSmNT_short <- dataSmNT[1:ifelse(num_nt <= length(dataSmNT), num_nt, length(dataSmNT))]
+          queryDown <- GDCquery(project = p, 
                             data.category = "Transcriptome Profiling",
                             data.type = "Gene Expression Quantification", 
                             workflow.type = "STAR - Counts", 
                             barcode = c(dataSmTP_short, dataSmNT_short))
-        } 
+          } 
+        else{
+          dataSmTP_short <- dataSmTP[1:length(dataSmTP)]
+          dataSmNT_short <- dataSmNT[1:length(dataSmNT)]
+          queryDown <- GDCquery(project = p, 
+                            data.category = "Transcriptome Profiling",
+                            data.type = "Gene Expression Quantification", 
+                            workflow.type = "STAR - Counts", 
+                            barcode = c(dataSmTP_short, dataSmNT_short))
+          }} 
       else{
-        dataSmTP_short <- dataSmTP[1:length(dataSmTP)]
-        dataSmNT_short <- dataSmNT[1:length(dataSmNT)]
+        dataSmTP_short <- dataSmTP
+        dataSmNT_short <- dataSmNT
         queryDown <- GDCquery(project = p, 
-                            data.category = "Transcriptome Profiling",
-                            data.type = "Gene Expression Quantification", 
-                            workflow.type = "STAR - Counts", 
-                            barcode = c(dataSmTP_short, dataSmNT_short))
-        }} 
-    else{
-      dataSmTP_short <- dataSmTP
-      dataSmNT_short <- dataSmNT
-      queryDown <- GDCquery(project = p, 
                             data.category = "Transcriptome Profiling",
                             data.type = "Gene Expression Quantification", 
                             workflow.type = "STAR - Counts", 
                             barcode = c(dataSmTP_short, dataSmNT_short))}
     
-    dataPrep1 <- GDCprepare(query = queryDown, directory = data_dir, save = save, save.filename = file.path("tmp", p, paste0(p,".rda")))
+      dataPrep1 <- GDCprepare(query = queryDown, directory = data_dir, save = save, save.filename = file.path("tmp", p, paste0(p,".rda")))
       
-    #a step to remove sample outliers using pearson correlation
-    #rownames(dataPrep1) <- rowData(dataPrep1)$gene_name #transfer to gene names
-    dataPrep <- TCGAanalyze_Preprocessing(object = dataPrep1, 
+      #a step to remove sample outliers using pearson correlation
+      #rownames(dataPrep1) <- rowData(dataPrep1)$gene_name #transfer to gene names
+      dataPrep <- TCGAanalyze_Preprocessing(object = dataPrep1, 
                                             cor.cut = 0.6,)
     
-    #step with library size and gcContent normalization using EDASeq
-    dataNorm <- TCGAanalyze_Normalization(tabDF = dataPrep,
+      #step with library size and gcContent normalization using EDASeq
+      dataNorm <- TCGAanalyze_Normalization(tabDF = dataPrep,
                                             geneInfo = geneInfoHT,
                                             method = "gcContent")
-    dataNorm <- TCGAanalyze_Normalization(tabDF = dataPrep,
+      dataNorm <- TCGAanalyze_Normalization(tabDF = dataPrep,
                                             geneInfo = geneInfoHT,
                                             method = "geneLength")
     
-    #quantile filtering to remove genes with low count
-    dataFilt <- TCGAanalyze_Filtering(tabDF = dataNorm,
+      #quantile filtering to remove genes with low count
+      dataFilt <- TCGAanalyze_Filtering(tabDF = dataNorm,
                                         method = "quantile", 
                                         qnt.cut =  0.25)
 
-    id2s <- as_tidytable(data.frame(rowData(dataPrep1))) %>% select(gene_id,gene_name) %>% mutate(gene_id = stringr::str_remove(gene_id, "\\..*")) 
-    id2s <- id2s %>% filter(gene_id %in% rownames(dataFilt)) %>% distinct(gene_name, .keep_all = T)
-    dataFilt <- dataFilt[id2s$gene_id,] #filter the genes with redundancy gene names
-    #make sure the order
-    if (all(rownames(dataFilt) == id2s$gene_id)){
-      rownames(dataFilt) <- id2s$gene_name
-      }
-    else {
-      print("Order is Wrong!!!")
-      }
+      id2s <- as_tidytable(data.frame(rowData(dataPrep1))) %>% select(gene_id,gene_name) %>% mutate(gene_id = stringr::str_remove(gene_id, "\\..*")) 
+      id2s <- id2s %>% filter(gene_id %in% rownames(dataFilt)) %>% distinct(gene_name, .keep_all = T)
+      dataFilt <- dataFilt[id2s$gene_id,] #filter the genes with redundancy gene names
+      #make sure the order
+      if (all(rownames(dataFilt) == id2s$gene_id)){
+        rownames(dataFilt) <- id2s$gene_name
+        }
+      else {
+        print("Order is Wrong!!!")
+        }
 
-    #voom transformation of the data (log)
-    v.dataFilt<-voom(dataFilt)
-    #taking log transformed data for exploration of batch effects
-    #c.dataFilt <- TCGAbatch_Correction(tabDF = v.dataFilt, batch.factor="Plate", adjustment=c("TSS"), is_plot=FALSE)
-    c.dataFilt <- v.dataFilt$E #初始化为voom转换矩阵，确保后续代码可以继续运行
-    tryCatch({
-      c.dataFilt <- TCGAbatch_Correction(tabDF = v.dataFilt, batch.factor = "Plate", adjustment = "TSS", is_plot = FALSE)
-      }, error = function(e) {
-      #Catch and skip
-      message("TCGAbatch_Correction error", e$message)
-      message("Skip!")
-    })
-    print("Continue!")
-    if (length(dataSmNT) > 3 ){
-      if (candidate %in% rownames(c.dataFilt)){
-        #survival 
-        clin <- GDCquery_clinic(p,"clinical")
-        texp <- c.dataFilt[candidate,dataSmTP_short]
-        cutoff <- median(texp) #use the median, so 50% high + 50% low 
-        group <- ifelse(texp > cutoff, "High", "Low")
-        group <- as_tidytable(data.frame(group), .keep_rownames = "barcode")
-        barcode <- as_tidytable(data.frame(colData(dataPrep1))) %>% select(barcode,patient) %>% filter(barcode %in% dataSmTP_short) %>% inner_join(group,by="barcode")
-        colnames(barcode)[2] <- "submitter_id"
-        df_clin <- barcode %>% inner_join(clin,by="submitter_id")
-        TCGAanalyze_survival(data.frame(df_clin), clusterCol="group", legend=candidate, main = paste("Kaplan-Meier Overall Survival Curves of", p), 
+      #voom transformation of the data (log)
+      v.dataFilt<-voom(dataFilt)
+      #taking log transformed data for exploration of batch effects
+      #c.dataFilt <- TCGAbatch_Correction(tabDF = v.dataFilt, batch.factor="Plate", adjustment=c("TSS"), is_plot=FALSE)
+      c.dataFilt <- v.dataFilt$E #初始化为voom转换矩阵，确保后续代码可以继续运行
+      tryCatch({
+        c.dataFilt <- TCGAbatch_Correction(tabDF = v.dataFilt, batch.factor = "Plate", adjustment = "TSS", is_plot = FALSE)
+        }, error = function(e) {
+        #Catch and skip
+        message("TCGAbatch_Correction error", e$message)
+        message("Skip!")
+      })
+      print("Continue!")
+      if (length(dataSmNT) > 3 ){
+        if (candidate %in% rownames(c.dataFilt)){
+          #survival 
+          clin <- GDCquery_clinic(p,"clinical")
+          texp <- c.dataFilt[candidate,dataSmTP_short]
+          cutoff <- median(texp) #use the median, so 50% high + 50% low 
+          group <- ifelse(texp > cutoff, "High", "Low")
+          group <- as_tidytable(data.frame(group), .keep_rownames = "barcode")
+          barcode <- as_tidytable(data.frame(colData(dataPrep1))) %>% select(barcode,patient) %>% filter(barcode %in% dataSmTP_short) %>% inner_join(group,by="barcode")
+          colnames(barcode)[2] <- "submitter_id"
+          df_clin <- barcode %>% inner_join(clin,by="submitter_id")
+          TCGAanalyze_survival(data.frame(df_clin), clusterCol="group", legend=candidate, main = paste("Kaplan-Meier Overall Survival Curves of", p), 
                              filename = file.path("tmp", p, paste0(p, ".pdf")))
-        if (length(target) > 0){
-          select_row = c(rownames(c.dataFilt)[1:2], target)}
-        else {
-          select_row = rownames(c.dataFilt)
-          }
-        DEG <- TCGAanalyze_DEA(
-          #add c(rownames(c.dataFilt)[1:2], target) avoid wrong of data format
-          mat1=c.dataFilt[select_row, dataSmNT_short], 
-          mat2=c.dataFilt[select_row, dataSmTP_short],
-          pipeline="limma",
-          Cond1type = "Normal",
-          Cond2type = "Tumor",
-          method = "glmLRT")
-        # 绘制分组箱线图并叠加点和标注
-        pvalue <- DEG[candidate,]$P.Value
-        plegend <- ifelse(pvalue < 0.0001, "****",
+          if (length(target) > 0){
+            select_row = c(rownames(c.dataFilt)[1:2], target)}
+          else {
+            select_row = rownames(c.dataFilt)
+            }
+          DEG <- TCGAanalyze_DEA(
+            #add c(rownames(c.dataFilt)[1:2], target) avoid wrong of data format
+            mat1=c.dataFilt[select_row, dataSmNT_short], 
+            mat2=c.dataFilt[select_row, dataSmTP_short],
+            pipeline="limma",
+            Cond1type = "Normal",
+            Cond2type = "Tumor",
+            method = "glmLRT")
+          # 绘制分组箱线图并叠加点和标注
+          pvalue <- DEG[candidate,]$P.Value
+          plegend <- ifelse(pvalue < 0.0001, "****",
                           ifelse(pvalue< 0.001, "***",
                             ifelse(pvalue < 0.01, "**",
                               ifelse(pvalue < 0.05, "*", "ns"))))
-        expression <- c.dataFilt[candidate,c(dataSmTP_short,dataSmNT_short)]
-        exp_data <- data.frame(expression); exp_data$sample <- rownames(exp_data)
-        exp_data <- exp_data %>%
-            mutate(group = ifelse(sample %in% dataSmTP_short, "tumor",
+          expression <- c.dataFilt[candidate,c(dataSmTP_short,dataSmNT_short)]
+          exp_data <- data.frame(expression); exp_data$sample <- rownames(exp_data)
+          exp_data <- exp_data %>%
+              mutate(group = ifelse(sample %in% dataSmTP_short, "tumor",
                             ifelse(sample %in% dataSmNT_short, "normal", NA)))
     
-        pp <- ggplot(exp_data, aes(x = group, y = expression, fill = group)) +
-        geom_boxplot(outlier.shape = NA, alpha = 0.7) +  # 不显示异常值，使叠加更清晰
-        geom_jitter(width = 0.2, aes(color = group), size = 2) +  # 叠加点，增加抖动防止重叠
-        labs(
-          title = paste("Gene Expression of Cancer vs Normal Samples in", p, "\n", "\n", "\n", plegend),
-          x = "Sample Group",
-          y = "Corrected Voom-transform Value"
-        ) +
-        theme_minimal() +
-        theme(plot.title = element_text(hjust = 0.5, size = 14))  # 隐藏图例（可选）
-        ggsave(file.path("tmp", p, paste0(p, "_Gene_Expression_Boxplot.pdf")), plot = pp, width = 8, height = 6, dpi = 600)
+          pp <- ggplot(exp_data, aes(x = group, y = expression, fill = group)) +
+          geom_boxplot(outlier.shape = NA, alpha = 0.7) +  # 不显示异常值，使叠加更清晰
+          geom_jitter(width = 0.2, aes(color = group), size = 2) +  # 叠加点，增加抖动防止重叠
+          labs(
+            title = paste("Gene Expression of Cancer vs Normal Samples in", p, "\n", "\n", "\n", plegend),
+            x = "Sample Group",
+            y = "Corrected Voom-transform Value"
+          ) +
+          theme_minimal() +
+          theme(plot.title = element_text(hjust = 0.5, size = 14))  # 隐藏图例（可选）
+          ggsave(file.path("tmp", p, paste0(p, "_Gene_Expression_Boxplot.pdf")), plot = pp, width = 8, height = 6, dpi = 600)
       
-      }
-      else {
-        if (length(target) > 1){
-            select_row = c(rownames(c.dataFilt)[1:2], target)}
+        }
         else {
-            select_row = rownames(c.dataFilt)
-            }
-        DEG <- TCGAanalyze_DEA(
-          mat1=c.dataFilt[select_row, dataSmNT_short], 
-          mat2=c.dataFilt[select_row, dataSmTP_short],
-          pipeline="limma",
-          Cond1type = "Normal",
-          Cond2type = "Tumor",
-          method = "glmLRT")
-      }
+          if (length(target) > 1){
+              select_row = c(rownames(c.dataFilt)[1:2], target)}
+          else {
+              select_row = rownames(c.dataFilt)
+              }
+          DEG <- TCGAanalyze_DEA(
+            mat1=c.dataFilt[select_row, dataSmNT_short], 
+            mat2=c.dataFilt[select_row, dataSmTP_short],
+            pipeline="limma",
+            Cond1type = "Normal",
+            Cond2type = "Tumor",
+            method = "glmLRT")
+        }
       
-      fwrite(as_tidytable(DEG, .keep_rownames = "gene_name"), file.path("tmp", p, paste0(p,"_deg.csv")))
+        fwrite(as_tidytable(DEG, .keep_rownames = "gene_name"), file.path("tmp", p, paste0(p,"_deg.csv")))
+        }
+      else {
+        print(paste(p, "It doesn't have enough normal samples!"))
+        }
+      tmp_mat <- as_tidytable(c.dataFilt, .keep_rownames = "gene_name")
+      fwrite(tmp_mat, file.path("tmp", p, paste0(p,"_exp.csv")))
+    
+      results[[paste0(p, "_exp")]] <- c.dataFilt
+      results[[paste0(p, "_deg")]] <- tmp_mat
+      
+      
+      
       }
     else {
-      print(paste(p, "It doesn't have enough normal samples!"))
-      }
-    tmp_mat <- as_tidytable(c.dataFilt, .keep_rownames = "gene_name")
-    fwrite(tmp_mat, file.path("tmp", p, paste0(p,"_exp.csv")))
-    
-    results[[paste0(p, "_exp")]] <- c.dataFilt
-    results[[paste0(p, "_deg")]] <- tmp_mat
+      print(paste(p, "is OK!"))
     }
-  
-  return(results)
-  }
+        
+      } #for end
+    return(results)
+    }
 #test
 #linshi <- mat2plot(data_dir = "../GDCdata/", is_short = TRUE)
 #project=c("TCGA-LUSC"); data_dir="../GDCdata"; num_tp=100; num_nt=100;tp_t="TP"; tp_n="NT"; 
