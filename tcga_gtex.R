4b8f6950d554e7e0df0a6c2de75063b15ca1a9b3
Qin Lin <47169253+Ci-TJ@users.noreply.github.com>
2025-03-31 17:16:35 +0800

    Update tcga_gtex.R

--- tcga_gtex.R
+++ tcga_gtex.R
@@ -39,7 +39,7 @@ print(modified_string)
       dir.create(file.path("tmp", p), recursive = TRUE)
     }
     #######
-    if (file.exists(file.path("tmp", p, paste0(p,"_exp.csv"))) == FALSE){
+    if (file.exists(file.path("tmp", p, paste0(p,"_gtex_exp.csv"))) == FALSE){
       query <- GDCquery(project = p,
                         data.category = "Transcriptome Profiling",
                         data.type = "Gene Expression Quantification", 
@@ -80,7 +80,7 @@ print(modified_string)
                               workflow.type = "STAR - Counts", 
                               barcode = c(dataSmTP_short, dataSmNT_short))}
     
-      dataPrep1 <- GDCprepare(query = queryDown, directory = data_dir, save = save, save.filename = file.path("tmp", p, paste0(p,".rda")))
+      dataPrep1 <- GDCprepare(query = queryDown, directory = data_dir, save = save, save.filename = file.path("tmp", p, paste0(p,"_gtex.rda")))
       
       #a step to remove sample outliers using pearson correlation
       #rownames(dataPrep1) <- rowData(dataPrep1)$gene_name #transfer to gene names
@@ -163,81 +163,34 @@ print(modified_string)
       AnnotationCounts[colnames(c.dataFilt),"Batch"] <- "TCGA"
       AnnotationCounts[colnames(gtex_normal),"Batch"] <- "GTEX"
 
-      AnnotationCounts[dataSmNT_short,"Condition"] <- "Normal"
+      if (length(dataSmNT_short) > 1){
+        AnnotationCounts[dataSmNT_short,"Condition"] <- "Normal"
+        }
       AnnotationCounts[dataSmTP_short,"Condition"] <- "Tumor"
       AnnotationCounts[colnames(gtex_normal),"Condition"] <- "Normal"
 
       #AnnotationCounts需要三列，Condition（协变量，可以理解成生物学条件）Batch（批次，这里主要指平台，tcga和gtex）Samples（样本id）
-      countsCorrected <- TCGAbatch_Correction(tabDF = countsTable,
-                                              UnpublishedData = TRUE, 
-                                              AnnotationDF = AnnotationCounts)
-      
-      
-      
-      
-
-
-
-      
-      
-      
-
-
-
       #############################
       ###tgex
       #############################
-      
-
-
-      
-
-
-      
       tryCatch({
-        c.dataFilt <- TCGAbatch_Correction(tabDF = v.dataFilt, batch.factor = "Plate", adjustment = "TSS", is_plot = FALSE)
+        c.dataFilt <- TCGAbatch_Correction(tabDF = countsTable,
+                                              UnpublishedData = TRUE, 
+                                              AnnotationDF = AnnotationCounts)
+        # c.dataFilt <- TCGAbatch_Correction(tabDF = v.dataFilt, batch.factor = "Plate", adjustment = "TSS", is_plot = FALSE)
         }, error = function(e) {
         #Catch and skip
         message("TCGAbatch_Correction error", e$message)
         message("Skip!")
       })
       print("Continue!")
+      ############################################################################
+      dataSmNT_short <- c(colnames(gtex_normal), dataSmNT_short) #Add 20250331
+      ############################################################################
       ##
-      if (length(dataSmNT) > 3 ){
+      if (nol(c.dataFilt) - length(dataSmTP_short) > 3 ){
         ###
         if (candidate %in% rownames(c.dataFilt)){
-          #survival 
-          clin <- GDCquery_clinic(p,"clinical")
-          flup <- fread(file.path(data_dir,"clinic_data", paste0(p, "_follow_up.tsv"))) #Note the file path!
-          texp <- c.dataFilt[candidate,dataSmTP_short]
-          cutoff <- median(texp) #use the median, so 50% high + 50% low 
-          group <- ifelse(texp > cutoff, "High", "Low")
-          group <- as_tidytable(data.frame(group), .keep_rownames = "barcode")
-          barcode <- as_tidytable(data.frame(colData(dataPrep1))) %>% select(barcode,patient) %>% filter(barcode %in% dataSmTP_short) %>% inner_join(group,by="barcode")
-          colnames(barcode)[2] <- "submitter_id"
-          df_clin <- barcode %>% inner_join(clin,by="submitter_id")
-          ##############
-          for (i in 1:nrow(df_clin)){
-            id <- df_clin$submitter_id[i]
-            if (id %in% flup$cases.submitter_id){
-              #print(paste(id,"in followup!",'\n'))
-              fdf <- flup %>% filter(cases.submitter_id == id)
-              days_followup <- as.numeric(fdf$follow_ups.days_to_follow_up)
-              #print(days_followup)
-              days_followup <- days_followup[!is.na(days_followup)]
-              #print(days_followup)
-              if (length(days_followup) >1){
-                last_day_followup <- max(days_followup)
-                df_clin$days_to_last_follow_up[i] <- last_day_followup
-              }
-            }
-            else {
-              print(paste(id, "is not in follow table!", "\n"))
-            }
-          }
-          ##############
-          TCGAanalyze_survival(data.frame(df_clin), clusterCol="group", legend=candidate, main = paste("Kaplan-Meier Overall Survival Curves of", p), 
-                               filename = file.path("tmp", p, paste0(p, ".pdf")))
           if (length(target) > 0){
             select_row = c(rownames(c.dataFilt)[1:2], target)
           }
@@ -275,7 +228,7 @@ print(modified_string)
           ) +
           theme_minimal() +
           theme(plot.title = element_text(hjust = 0.5, size = 14))  # 隐藏图例（可选）
-          ggsave(file.path("tmp", p, paste0(p, "_Gene_Expression_Boxplot.pdf")), plot = pp, width = 8, height = 6, dpi = 600)
+          ggsave(file.path("tmp", p, paste0(p, "_gtex_Gene_Expression_Boxplot.pdf")), plot = pp, width = 8, height = 6, dpi = 600)
       
         } ###
         else {
@@ -293,13 +246,13 @@ print(modified_string)
             method = "glmLRT")
         }
       
-        fwrite(as_tidytable(DEG, .keep_rownames = "gene_name"), file.path("tmp", p, paste0(p,"_deg.csv")))
+        fwrite(as_tidytable(DEG, .keep_rownames = "gene_name"), file.path("tmp", p, paste0(p,"_gtex_deg.csv")))
         }##
       else {
         print(paste(p, "It doesn't have enough normal samples!"))
         }
       tmp_mat <- as_tidytable(c.dataFilt, .keep_rownames = "gene_name")
-      fwrite(tmp_mat, file.path("tmp", p, paste0(p,"_exp.csv")))
+      fwrite(tmp_mat, file.path("tmp", p, paste0(p,"_gtex_exp.csv")))
     
       results[[paste0(p, "_exp")]] <- c.dataFilt
       results[[paste0(p, "_deg")]] <- tmp_mat
